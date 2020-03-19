d4b6364f5aef1f1035ba3266655951be64c7800b
Kernel <nbelsito02@gmail.com>
2020-03-19 14:34:47 -0400

    Test 3

--- src/main.rs
+++ src/main.rs
@@ -206,14 +206,64 @@ fn fib(n: i32) -> i32 {
     fib(n - 1) + fib(n - 2)
 }
 
-fn sing() { // wip
-    let days = [
-        "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth",
-        "tenth", "eleventh", "twelfth",
-    ];
+fn sing() {
+    fn day_intro(n: u32) {
+        let day = match n {
+            1 => "first",
+            2 => "second",
+            3 => "third",
+            4 => "fourth",
+            5 => "fifth",
+            6 => "sixth",
+            7 => "seventh",
+            8 => "eighth",
+            9 => "ninth",
+            10 => "tenth",
+            11 => "eleventh",
+            12 => "twelfth",
+            _ => "",
+        };
 
-    for day in days.iter() {
-        println!("On the {} day of Christmas", day);
-        println!("My true love gave to me");
+        println!(
+            "\nOn the {} day of Christmas\nMy true love gave to me:",
+            day
+        );
+    }
+
+    fn gift(n: u32, prefix: &str) {
+        let gift_text = match n {
+            1 => "a Partridge in a Pear Tree",
+            2 => "Two Turtle Doves",
+            3 => "Three French Hens",
+            4 => "Four Calling Birds",
+            5 => "Five Golden Rings",
+            6 => "Six Geese a Laying",
+            7 => "Seven Swans a Swimming",
+            8 => "Eight Maids a Milking",
+            9 => "Nine Ladies Dancing",
+            10 => "Ten Lords a Leaping",
+            11 => "Eleven Pipers Piping",
+            12 => "12 Drummers Drumming",
+            _ => "",
+        };
+
+        println!("{}{}", prefix, gift_text);
+    }
+
+    println!("\n\nTwelve Days Of Christmas: ");
+
+    for day in 1..13 {
+        day_intro(day);
+
+        for gift_day in (1..(day + 1)).rev() {
+            gift(
+                gift_day,
+                if gift_day == 1 && day != 1 {
+                    "and "
+                } else {
+                    ""
+                },
+            );
+        }
     }
 }
